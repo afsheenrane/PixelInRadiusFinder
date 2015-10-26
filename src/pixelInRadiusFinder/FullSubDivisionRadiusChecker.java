3f6920245bea3eeb7f015e62d2af4fc5742546c9
afsheen <afsheen_rane@hotmail.com>
2015-10-26 02:03:10 -0400

    Fixed the code so that it actually works.
    Increased initial capacity of arraylist and removed pt and replaced it
    with manual array creation.

--- src/pixelInRadiusFinder/FullSubDivisionRadiusChecker.java
+++ src/pixelInRadiusFinder/FullSubDivisionRadiusChecker.java
@@ -5,46 +5,28 @@ import java.util.ArrayList;
 public class FullSubDivisionRadiusChecker {
 
     public static ArrayList<int[]> getAllPoints(int[] vertex, int r) {
-        ArrayList<int[]> allPts = new ArrayList<int[]>((int) (Math.PI * r * r));
+        ArrayList<int[]> allPts = new ArrayList<int[]>((int) (Math.PI * r * r * 1.5));
 
         int rSq = r * r;
-
-        int[] pt = new int[2];
 
         for (int x = 0; x <= r; x++) {
             for (int y = 0; y <= x; y++) {
                 if ((x * x) + (y * y) <= rSq) {
-                    pt[0] = x + vertex[0];
-                    pt[1] = y + vertex[1];
-                    allPts.add(pt);
+                    allPts.add(new int[] { x + vertex[0], y + vertex[1] });
 
-                    pt[0] = y + vertex[0];
-                    pt[1] = x + vertex[1];
-                    allPts.add(pt);
+                    allPts.add(new int[] { y + vertex[0], x + vertex[1] });
 
-                    pt[0] = -y + vertex[0];
-                    pt[1] = x + vertex[1];
-                    allPts.add(pt);
+                    allPts.add(new int[] { -y + vertex[0], x + vertex[1] });
 
-                    pt[0] = -x + vertex[0];
-                    pt[1] = y + vertex[1];
-                    allPts.add(pt);
+                    allPts.add(new int[] { -x + vertex[0], y + vertex[1] });
 
-                    pt[0] = -x + vertex[0];
-                    pt[1] = -y + vertex[1];
-                    allPts.add(pt);
+                    allPts.add(new int[] { -x + vertex[0], -y + vertex[1] });
 
-                    pt[0] = -y + vertex[0];
-                    pt[1] = -x + vertex[1];
-                    allPts.add(pt);
+                    allPts.add(new int[] { -y + vertex[0], -x + vertex[1] });
 
-                    pt[0] = y + vertex[0];
-                    pt[1] = -x + vertex[1];
-                    allPts.add(pt);
+                    allPts.add(new int[] { y + vertex[0], -x + vertex[1] });
 
-                    pt[0] = x + vertex[0];
-                    pt[1] = -y + vertex[1];
-                    allPts.add(pt);
+                    allPts.add(new int[] { x + vertex[0], -y + vertex[1] });
 
                     for (int g = 0; g < 16; g++) {
                         double k = Math.sqrt(x * x + y * y);
