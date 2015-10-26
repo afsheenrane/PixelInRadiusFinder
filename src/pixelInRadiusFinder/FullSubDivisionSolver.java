4d31de968feb20c76141b830d7c2fad9f35f78af
afsheen <afsheen_rane@hotmail.com>
2015-10-26 01:58:31 -0400

    Reduced greatly increased efficiency of code (and it actually works now)
    Previous code was actually broken, because the entire list pointed to
    pt, instead of becoming a copy of it's value. Now, new arrays are
    manually assigned per point (manual assignment came out to be the
    fastest in tests). Also reduced the amount of overlap, and increased the
    initial capacity of the arraylist (this actually helped quite a bit).

--- src/pixelInRadiusFinder/FullSubDivisionSolver.java
+++ src/pixelInRadiusFinder/FullSubDivisionSolver.java
@@ -5,51 +5,30 @@ import java.util.ArrayList;
 public class FullSubDivisionSolver {
 
     public static ArrayList<int[]> getAllPoints(int[] vertex, int r) {
-        ArrayList<int[]> allPts = new ArrayList<int[]>((int) (Math.PI * r * r));
-
+        ArrayList<int[]> allPts = new ArrayList<int[]>((int) (Math.PI * r * r * 1.5));
         int endPt = (int) (r / Math.sqrt(2));
 
-        int[] pt = new int[2];
+        // The cross!
+        for (int x = 0; x <= endPt; x++) {
+            allPts.add(new int[] { x + vertex[0], vertex[1] });
 
-        // Add in central cross that gets missed out.
-        pt[0] = 0;
-        pt[1] = 0;
-        allPts.add(pt);
+            allPts.add(new int[] { vertex[0], x + vertex[1] });
 
-        pt[0] = 1;
-        pt[1] = 0;
-        allPts.add(pt);
+            allPts.add(new int[] { -x + vertex[0], vertex[1] });
 
-        pt[0] = 0;
-        pt[1] = 1;
-        allPts.add(pt);
-
-        pt[0] = -1;
-        pt[1] = 0;
-        allPts.add(pt);
-
-        pt[0] = 0;
-        pt[1] = -1;
-        allPts.add(pt);
+            allPts.add(new int[] { vertex[0], -x + vertex[1] });
+        }
 
         // First solve the central square.
         for (int x = 1; x <= endPt; x++) {
             for (int y = 1; y <= endPt; y++) {
-                pt[0] = x + vertex[0];
-                pt[1] = y + vertex[1];
-                allPts.add(pt);
+                allPts.add(new int[] { x + vertex[0], y + vertex[1] });
 
-                pt[0] = -x + vertex[0];
-                pt[1] = y + vertex[1];
-                allPts.add(pt);
+                allPts.add(new int[] { -x + vertex[0], y + vertex[1] });
 
-                pt[0] = x + vertex[0];
-                pt[1] = -y + vertex[1];
-                allPts.add(pt);
+                allPts.add(new int[] { x + vertex[0], -y + vertex[1] });
 
-                pt[0] = -x + vertex[0];
-                pt[1] = -y + vertex[1];
-                allPts.add(pt);
+                allPts.add(new int[] { -x + vertex[0], -y + vertex[1] });
 
                 // Just to simulate scenarios where each point's distance from
                 // the center would like to be known. This has a negligible
@@ -61,7 +40,7 @@ public class FullSubDivisionSolver {
         }
 
         // Then solve the half semi circles that remain.
-        int startX = endPt;
+        int startX = endPt + 1;
         int endX = r;
 
         int endY;
@@ -71,37 +50,21 @@ public class FullSubDivisionSolver {
         for (int x = startX; x <= endX; x++) {
             endY = (int) Math.sqrt(rSq - (x * x));
             for (int y = 0; y <= endY; y++) {
-                pt[0] = x + vertex[0];
-                pt[1] = y + vertex[1];
-                allPts.add(pt);
+                allPts.add(new int[] { x + vertex[0], y + vertex[1] });
 
-                pt[0] = y + vertex[0];
-                pt[1] = x + vertex[1];
-                allPts.add(pt);
+                allPts.add(new int[] { y + vertex[0], x + vertex[1] });
 
-                pt[0] = -y + vertex[0];
-                pt[1] = x + vertex[1];
-                allPts.add(pt);
+                allPts.add(new int[] { -y + vertex[0], x + vertex[1] });
 
-                pt[0] = -x + vertex[0];
-                pt[1] = y + vertex[1];
-                allPts.add(pt);
+                allPts.add(new int[] { -x + vertex[0], y + vertex[1] });
 
-                pt[0] = -x + vertex[0];
-                pt[1] = -y + vertex[1];
-                allPts.add(pt);
+                allPts.add(new int[] { -x + vertex[0], -y + vertex[1] });
 
-                pt[0] = -y + vertex[0];
-                pt[1] = -x + vertex[1];
-                allPts.add(pt);
+                allPts.add(new int[] { -y + vertex[0], -x + vertex[1] });
 
-                pt[0] = y + vertex[0];
-                pt[1] = -x + vertex[1];
-                allPts.add(pt);
+                allPts.add(new int[] { y + vertex[0], -x + vertex[1] });
 
-                pt[0] = x + vertex[0];
-                pt[1] = -y + vertex[1];
-                allPts.add(pt);
+                allPts.add(new int[] { x + vertex[0], -y + vertex[1] });
 
                 // Just to simulate scenarios where each point's distance from
                 // the center would like to be known. This has a negligible
